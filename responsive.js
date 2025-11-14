e9a5f7cb60891dc37aa6f412d67771e82d5db248
PoojaVinodMane <131657642+PoojaVinodMane@users.noreply.github.com>
2025-11-15 03:44:23 +0530

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,23 @@
+/*function showSection(sectionId) {
+    const sections = document.getElementsByClassName("section-content");
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].style.display = "none";
+    }
+    document.getElementById(sectionId).style.display = "block";
+  }*/
+  showSection("homesection");
+  
+  function showSection(sectionId) {
+    // Hide all sections
+    var sections = document.getElementsByClassName("section-content");
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].style.display = "none";
+    }
+    
+    // Show the selected section
+    var section = document.getElementById(sectionId);
+    if (section) {
+      section.style.display = "block";
+    }
+  }
+  
